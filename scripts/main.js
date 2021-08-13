8cda4e97081a7432cfe0268a72e35a60100ef4a0
Sara Ghaedi <saraghaedi75@gmail.com>
2021-08-13 17:39:26 +0200

    fix: delete extra comments

--- scripts/main.js
+++ scripts/main.js
@@ -34,29 +34,20 @@ for (let i = 0; i < checkboxs.length; i++) {
 }
 
 function createTodo(title) {
-    // create a label
     const newLabel = document.createElement('label');
 
-    // create a checkbox
     const newCheckBox = document.createElement('input');
     newCheckBox.setAttribute("type", "checkbox");
     newCheckBox.checked = false;
 
-    // add the "change" event listener to the checkbox
     newCheckBox.addEventListener('change', toggleDone);
-    // and append the checkbox to the label
     newLabel.appendChild(newCheckBox);
 
-    // create a text node with the given title
     const newTitle = document.createTextNode(" " + title);
-    // and append the text node to the label
     newLabel.appendChild(newTitle);
 
-    // create a list item
     newListItem = document.createElement('li');
-    // and append the label to list item
     newListItem.appendChild(newLabel);
-    // append the list item to the todo list
     const ul = document.getElementById("tasks");
     newListItem.className = 'task todo';
     ul.appendChild(newListItem);
@@ -70,29 +61,17 @@ document
         const inputField = document.querySelector(".input-btn");
         const newTodoTitle = inputField.value;
         createTodo(newTodoTitle);
-
-        // reset the value of the inputField to make it empty and
-        // ready to create new todos
         inputField.value = null;
 
         updateCounters();
     });
 
 function cleanUpDoneTodos() {
-    // get all the "done" items
     const doneItems = document.querySelectorAll('.done');
-    // loop through the "done" todo items
     for (let i = 0; i < doneItems.length; i++) {
-        // and remove them from the DOM
         doneItems[i].remove();
     }
-
-    // update the counters
     updateCounters();
 }
-
-// retrieve the link
 const cleanButton = document.getElementById('clean-btn');
-// add an event listener for a click on the link to the cleanUpDoneTodos function
-
 cleanButton.addEventListener('click', cleanUpDoneTodos);
