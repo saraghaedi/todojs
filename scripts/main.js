bc74634ea99cf4fa3a743b99b7e7dd8adbf912ed
Sara Ghaedi <saraghaedi75@gmail.com>
2021-08-13 17:20:55 +0200

    Feat: Add link to cleanup completed todos

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,14 @@
 /*jshint esversion: 6 */
 
 function updateCounters() {
-    
+
     const total_count = document.getElementById('total-count');
     const todo_count = document.getElementById('todo-count');
     const completed_count = document.getElementById('completed-count');
     const todos = document.querySelectorAll('.todo');
     const done = document.querySelectorAll('.done');
     const task = document.getElementsByClassName('task');
-    
+
     todo_count.textContent = `Todo: ${todos.length}`;
     completed_count.textContent = `Done: ${done.length}`;
     total_count.textContent = `Total: ${task.length}`;
@@ -17,9 +17,9 @@ function updateCounters() {
 updateCounters();
 
 function toggleDone(event) {
-    
+
     const checkbox = event.currentTarget;
-    
+
     if (checkbox.checked) {
         checkbox.parentElement.parentElement.className = "done task";
     } else {
@@ -36,7 +36,7 @@ for (let i = 0; i < checkboxs.length; i++) {
 function createTodo(title) {
     // create a label
     const newLabel = document.createElement('label');
-  
+
     // create a checkbox
     const newCheckBox = document.createElement('input');
     newCheckBox.setAttribute("type", "checkbox");
@@ -46,12 +46,12 @@ function createTodo(title) {
     newCheckBox.addEventListener('change', toggleDone);
     // and append the checkbox to the label
     newLabel.appendChild(newCheckBox);
-  
+
     // create a text node with the given title
     const newTitle = document.createTextNode(" " + title);
     // and append the text node to the label
     newLabel.appendChild(newTitle);
-  
+
     // create a list item
     newListItem = document.createElement('li');
     // and append the label to list item
@@ -60,21 +60,39 @@ function createTodo(title) {
     const ul = document.getElementById("tasks");
     newListItem.className = 'task todo';
     ul.appendChild(newListItem);
-  }
-  
-  document
+}
+
+document
     .querySelector("form")
     .addEventListener("submit", function addNewTodo(event) {
-      event.preventDefault();
-  
-      const inputField = document.querySelector(".input-btn");
-      const newTodoTitle = inputField.value;
-      createTodo(newTodoTitle);
-  
-      // reset the value of the inputField to make it empty and
-      // ready to create new todos
-      inputField.value = null;
-  
-      updateCounters();
+        event.preventDefault();
+
+        const inputField = document.querySelector(".input-btn");
+        const newTodoTitle = inputField.value;
+        createTodo(newTodoTitle);
+
+        // reset the value of the inputField to make it empty and
+        // ready to create new todos
+        inputField.value = null;
+
+        updateCounters();
     });
 
+function cleanUpDoneTodos() {
+    // get all the "done" items
+    const doneItems = document.querySelectorAll('.done');
+    // loop through the "done" todo items
+    for (let i = 0; i < doneItems.length; i++) {
+        // and remove them from the DOM
+        doneItems[i].remove();
+    }
+
+    // update the counters
+    updateCounters();
+}
+
+// retrieve the link
+const cleanButton = document.getElementById('clean-btn');
+// add an event listener for a click on the link to the cleanUpDoneTodos function
+
+cleanButton.addEventListener('click', cleanUpDoneTodos);
