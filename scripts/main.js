284374f5a53e0cd688a5b9d6d406a84458a35218
Sara Ghaedi <saraghaedi75@gmail.com>
2021-08-13 15:23:51 +0200

    feat: Add toggledone  function

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,33 @@
 /*jshint esversion: 6 */
 
-const total_count = document.getElementById('total-count');
-const todo_count = document.getElementById('todo-count');
-const completed_count = document.getElementById('completed-count');
-
 function updateCounters() {
-
-    let todos = document.querySelectorAll('.undone-lbl');
-    let done = document.querySelectorAll('.done-lbl');
-
+    
+    const total_count = document.getElementById('total-count');
+    const todo_count = document.getElementById('todo-count');
+    const completed_count = document.getElementById('completed-count');
+    const todos = document.querySelectorAll('.todo');
+    const done = document.querySelectorAll('.done');
+    
     todo_count.textContent = `Todo: ${todos.length}`;
     completed_count.textContent = `Done: ${done.length}`;
     total_count.textContent = `Total: ${todos.length + done.length}`;
-  }
-  
-  updateCounters();
+}
+
+updateCounters();
+
+function toggleDone(event) {
+    
+    const checkbox = event.currentTarget;
+    
+    if (checkbox.checked) {
+        checkbox.parentElement.parentElement.className = "done task";
+    } else {
+        checkbox.parentElement.parentElement.className = "todo task";
+    }
+    updateCounters();
+}
+
+const checkboxs = document.querySelectorAll(".task input");
+for (let i = 0; i < checkboxs.length; i++) {
+    checkboxs[i].addEventListener("change", toggleDone);
+}
